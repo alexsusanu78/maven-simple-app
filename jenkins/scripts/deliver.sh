b1417bc25f8d45b95393c15e2bdfb5551e6d2e42
alexsusanu78 <alexsusanu78@gmail.com>
2025-11-27 13:13:50 +0200

    updated delivery script

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+MAVEN_LOCAL_REPO = '/tmp/maven-repo'
+
 echo 'The following Maven command installs your Maven-built Java application'
 echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
@@ -11,13 +13,13 @@ set +x
 echo 'The following command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
-NAME=`mvn -q -DforceStdout help:evaluate -Dexpression=project.name`
+NAME=`mvn -Dmaven.repo.local=${MAVEN_LOCAL_REPO} -q -DforceStdout help:evaluate -Dexpression=project.name`
 set +x
 
 echo 'The following command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
-VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`
+VERSION=`mvn -Dmaven.repo.local=${MAVEN_LOCAL_REPO} -q -DforceStdout help:evaluate -Dexpression=project.version`
 set +x
 
 echo 'The following command runs and outputs the execution of your Java'
